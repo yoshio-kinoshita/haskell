dc651be57725e3fd9e5302460bc5bb79acb26753
yoshio <yoshio@yz.velvet.jp>
2015-02-23 00:07:50 +0900

    baby.hs updated

--- baby.hs
+++ baby.hs
@@ -1,3 +1,11 @@
 doubleMe x = x + x
+
 doubleUs x y = x * 2 + y * 2
+
+doubleSamllNumber x = 
+    if x > 100 then x else x * 2
+
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
